7c0e7209ad14baa146cc071fef002e48e20480e0
berenice02 <foderaro.elisa@gmail.com>
2021-02-08 00:15:44 +0100

    rimossi i riferimenti al tutorial

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 # Run a test server.
 from app import app
-from flask import render_template
-
-@app.route('/')
-def main():
-    return render_template("menu.html")
 
 app.run(host='0.0.0.0', port=8080, debug=True)
